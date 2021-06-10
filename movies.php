71fcce8c00687ca413147d36bb2dfff3ea50f5c1
SangJoon Lee(Jeremy) <sangjoon.jeremy.lee@gmail.com>
2021-06-10 09:08:10 +0800

    Fix Responsive design

--- movies.php
+++ movies.php
@@ -6,6 +6,7 @@
     <head>
         <title>Web Programing Project</title>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
         <link rel="stylesheet" type="text/css" href="style.css?<?php echo time(); ?>" />
     </head>
     <body>
